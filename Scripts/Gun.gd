e535077a016a9acfffadc860641f1a6b5d64f158
Sheep26 <crabeykiller@gmail.com>
2023-11-09 20:18:15 +1300

    gun and bullets some code dont work pls help

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+var current_bullets = []
+@onready var bullet = preload("res://Objects/Bullet.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("fire"):
+		var bullet_instantiated = bullet.instantiate()
+		bullet_instantiated.set("rotation", rotation)
+		# HELP WHY IS THIS NOT WORKING
+		bullet_instantiated.set("transform.x", transform.x)
+		bullet_instantiated.set("transform.y", transform.y)
+		# end of not working
+		current_bullets.append(bullet_instantiated)
+		get_tree().current_scene.add_child(bullet_instantiated)
