037cd4d39d740e6753d20a6a585eefeaad5b0c95
SamBayot <sam.bayot09@gmail.com>
2023-11-10 09:12:10 +1300

    knockback
    x-axis doesn't work yet because of how the movement works on the player script

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+const KNOCKBACK_STRENGTH = 200
 var current_bullets = []
 @onready var bullet = preload("res://Objects/Bullet.tscn")
 
@@ -11,6 +11,7 @@ func _process(_delta):
 	var rotationToVector2 = Vector2(cos(rotation), sin(rotation))
 	rotate(get_angle_to(get_global_mouse_position()))
 	if Input.is_action_just_pressed("fire"):
+		get_parent().velocity = -(rotationToVector2 * KNOCKBACK_STRENGTH)
 		var bullet_instantiated = bullet.instantiate()
 		bullet_instantiated.rotation = rotation
 		bullet_instantiated.position = position + get_parent().position + rotationToVector2 * 94
