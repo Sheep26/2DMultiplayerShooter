be1d239438dbd5697757711591990251099a07bc
Sheep26 <crabeykiller@gmail.com>
2023-11-09 17:00:32 +1300

    started enemys

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+		
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
