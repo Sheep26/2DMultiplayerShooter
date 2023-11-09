7ba8f8656af742b34d926038b055d11a886ad1ef
Sheep26 <crabeykiller@gmail.com>
2023-11-09 20:56:28 +1300

    e

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -12,9 +12,7 @@ func _process(_delta):
 	if Input.is_action_just_pressed("fire"):
 		var bullet_instantiated = bullet.instantiate()
 		bullet_instantiated.set("rotation", rotation)
-		# HELP WHY IS THIS NOT WORKING
-		bullet_instantiated.set("transform.x", transform.x)
-		bullet_instantiated.set("transform.y", transform.y)
-		# end of not working
+		bullet_instantiated.set("position.x", position.x)
+		bullet_instantiated.set("position.y", position.y)
 		current_bullets.append(bullet_instantiated)
 		get_tree().current_scene.add_child(bullet_instantiated)
