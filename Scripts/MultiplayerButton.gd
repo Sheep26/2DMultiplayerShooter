1f3e14e576586093bed60c75794d17122dcf1e04
Sheep26 <crabeykiller@gmail.com>
2023-11-09 13:53:16 +1300

    _

--- Scripts/MultiplayerButton.gd
+++ Scripts/MultiplayerButton.gd
@@ -5,9 +5,8 @@ extends Button
 func _ready():
 	pressed.connect(self._button_pressed) # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 func _button_pressed():
