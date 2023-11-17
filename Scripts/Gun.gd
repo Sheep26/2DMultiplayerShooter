9f7d999e9fb9db6f7eeafbed4945af8d3b7e2734
Sheep26 <crabeykiller@gmail.com>
2023-11-17 15:08:37 +1300

    bing

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -3,11 +3,13 @@ const KNOCKBACK_STRENGTH = 400
 var currentGun: GunType
 @onready var bullet = preload("res://Objects/Bullet.tscn")
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var pistol: GunType = GunType.new("Pistol", $Guns/pistol/firerateTimer, $Guns/pistol/reloadTimer, 23, false)
-@onready var ak47: GunType = GunType.new("AK47", $Guns/ak47/firerateTimer, $Guns/ak47/reloadTimer, 35, true)
+@onready var glock18: GunType = GunType.new("Glock-18", "Pistol", "res://Assets/Glock18.png", $Guns/pistol/firerateTimer, $Guns/pistol/reloadTimer, 23, false)
+@onready var ak47: GunType = GunType.new("AK47", "Rifle", "res://Assets/AK47.png", $Guns/ak47/firerateTimer, $Guns/ak47/reloadTimer, 35, true)
 
 class GunType:
 	var gunName: String
+	var category: String
+	var artAssetPath: String
 	var firerate: Timer
 	var reloadTimer: Timer
 	var ammo: int
@@ -15,13 +17,14 @@ class GunType:
 	var auto: bool
 	var canShoot: bool
 	
-	func _init(gunNameArg: String, firerateArg: Timer, reloadTimerArg: Timer, ammoArg: int, autoArg):
+	func _init(gunNameArg: String, categoryArg: String, artAssetPath: String, firerateArg: Timer, reloadTimerArg: Timer, ammoArg: int, autoArg):
 		gunName = gunNameArg
 		firerate = firerateArg
 		reloadTimer = reloadTimerArg
 		ammo = ammoArg
 		maxAmmo = ammoArg
 		auto = autoArg
+		category = categoryArg
 		canShoot = true
 		firerate.connect("timeout", _firerateTimerEnded)
 		reloadTimer.connect("timeout", _reloadTimerEnded)
@@ -40,7 +43,7 @@ class GunType:
 		firerate.stop()
 
 func _ready():
-	currentGun = pistol
+	currentGun = glock18
 
 func _shoot(rotationToVector2):
 	get_parent().velocity.y = -(rotationToVector2.y * KNOCKBACK_STRENGTH) 
