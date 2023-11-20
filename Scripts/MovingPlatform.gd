c7f58d009ea143c2baa3cc1bc853bc0901b49d70
SamBayot <sam.bayot09@gmail.com>
2023-11-20 21:21:21 +1300

    yay moving platform

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -1,14 +1,19 @@
 extends StaticBody2D
 
 @export var speed:float
-@export var addX:float
-@export var addY:float
-var startX:float
-var startY:float
+@export var add = Vector2()
+var start = Vector2()
+var goto = true
 
 func _ready():
-	startX = position.x
-	startY = position.y
+	start = position
 
 func _process(delta):
-	position = position.move_toward(Vector2(startX+addX,startY+addY), delta * speed)
+	if goto:
+		position = position.move_toward(start, delta * speed)
+	else:
+		position = position.move_toward(start + add , delta * speed)
+	if position == start:
+		goto = false
+	elif position == start + add:
+		goto = true 
