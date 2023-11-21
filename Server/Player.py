e0e2dfe7a49d883b60758c1df25aaab9efa3c53b
Sheep26 <crabeykiller@gmail.com>
2023-11-22 10:27:43 +1300

    e

--- Server/Player.py
+++ Server/Player.py
@@ -1,5 +1,10 @@
 class Player:
+    name: str
     x: int
     y: int
     currentGunID: int
-    ID: int
+    id: int
+    
+    def __init__(self, name: str, id: str):
+        self.name = name
+        self.id = id
--- Server/Server.py
+++ Server/Server.py
@@ -1,7 +1,10 @@
-import flask
+from flask import Flask
+from flask import request
+from flask import Response
 import json
+from player import Player
 
-
+players = []
 
 with open("config.json", "r") as configFile:
     config = json.load(configFile)
@@ -9,10 +12,18 @@ with open("config.json", "r") as configFile:
 name: str = config["settings"]["serverName"]
 port: str = config["settings"]["serverPort"]
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def default():
     return name
+
+@app.route("/getPlayerFromID")
+def getPlayerFromID():
+    id = request.args["id"]
+    for player in players:
+        if player.id == id:
+            return player
+    return ""
 
 app.run("0.0.0.0", port)
