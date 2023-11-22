ea3e83a024c90e771849d051f5508c848dcef71d
Sheep26 <crabeykiller@gmail.com>
2023-11-22 14:54:37 +1300

    server stuff

--- Server/maps.py
+++ Server/maps.py
@@ -2,10 +2,15 @@ class Map:
     name: str
     id: int
     path: str
+    spawnX: int
+    spawnY: int
     
-    def __init__(self, name: str, id: int, path: str):
+    def __init__(self, name: str, id: int, path: str, spawnX: int, spawnY: int):
         self.name = name
         self.id = id
         self.path = path
+        self.spawnX = spawnX
+        self.spawnY = spawnY
         
-level1: Map = Map("level1", 1, "")
+level1: Map = Map("level1", 1, "", 20, 20)
+Maps = [level1]
--- Server/server.py
+++ Server/server.py
@@ -5,9 +5,11 @@ import json
 from player import Player
 from random import randint
 from maps import Map
+from maps import Maps
 
 players = []
 map: Map
+map = Maps[0]
 
 with open("config.json", "r") as configFile:
     config = json.load(configFile)
@@ -21,6 +23,16 @@ app = Flask(__name__)
 def default():
     Response(name, 200)
 
+@app.route("/setMap")
+def setMap():
+    mapName = request.args["map"]
+    for map1 in Maps:
+        if map1.name == mapName:
+            map = map1
+            
+def getMap():
+    return Response(f"{map.path}")
+
 @app.route("/join")
 def join():
     playerName = request.args["name"]
@@ -29,12 +41,21 @@ def join():
         id += str(randint(0, 9))
     player = Player(playerName, id, map)
     players.append(player)
-    return Response(f"{player.name}:{player.id}:{player.map}:{player.x}:{player.y}", 200)
+    return Response(f"{player.name}:{player.id}:{map.name}:{map.path}:{player.x}:{player.y}", status=200)
+
+@app.route("/getPlayers")
+def getPlayers():
+    returnList = []
+    for player in players:
+        returnList.append(f"{player.name}:{player.id}:{player.x}:{player.y}:{player.currentGunID}")
+    return Response(str(returnList).replace('\'', "").replace("[", "").replace("]", "").replace(",", "\n"))
 
 @app.route("/getPlayerFromID")
 def getPlayerFromID():
     id = request.args["id"]
     for player in players:
         if player.id == id:
-            Response(player, 200)
+            Response(f"{player.name}:{player.id}:{player.x}:{player.y}:{player.currentGunID}", status=200)
     return Response(status=404)
+
+app.run("0.0.0.0", port)
