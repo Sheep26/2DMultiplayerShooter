8fd5c47364ee3e2fe1d888207e2b4851b90c66d0
Sheep26 <crabeykiller@gmail.com>
2023-11-22 15:54:48 +1300

    server stuff

---
+++ Scripts/MultiplayerUI.gd
@@ -0,0 +1,5 @@
+extends Control
+@onready var TextBox: TextEdit = $TextEdit
+
+func _on_button_pressed():
+	pass
--- Server/player.py
+++ Server/player.py
@@ -1,4 +1,6 @@
 from maps import Map
+from time import time
+from requests import get
 
 class Player:
     name: str
@@ -7,11 +9,36 @@ class Player:
     currentGunID: int
     id: int
     map: Map
+    ip: str
+    alive: int
     
-    def __init__(self, name: str, id: str, map: Map):
+    def __init__(self, name: str, id: str, ip: str, map: Map):
         self.name = name
         self.id = id
         self.map = map
         self.x = 0
         self.y = 0
         self.currentGunID = 1
+        self.ip = ip
+        self.alive = True
+        
+    def setAlive(self):
+        self.alive = time()
+        
+    def getAlive(self) -> int:
+        return self.alive
+    
+    def sendPacket(self, packet: str):
+        get(f"{self.ip}/packet")
+        
+    def setX(self, x: int):
+        self.x = x
+    
+    def setY(self, y: int):
+        self.y = y
+    
+    def getX(self) -> int:
+        return self.x
+    
+    def getY(self) -> int:
+        return self.y
